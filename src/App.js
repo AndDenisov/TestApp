0a4c403e1eac26039570201ebbf5001e29def304
ЧИ <denisov_ad@astralnalog.ru>
2018-10-17 23:03:27 +0300

    адаптировал таблицу под заданные поля

--- src/App.js
+++ src/App.js
@@ -1,9 +1,99 @@
 import React, { Component } from 'react';
 import { AdvancedTable } from "./components/AdvancedTable";
 
+const file = JSON.stringify({
+    "pages": 184,
+    "arguments": null,
+    "clusters": null,
+    "per_page": 1,
+    "alternate_url": "https://hh.ru/search/vacancy?area=43&specialization=1&items_on_page=1&enable_snippets=true",
+    "page": 0,
+    "found": 184,
+    "items": [
+        {
+            "area": {
+                "url": "https://api.hh.ru/areas/43",
+                "id": "43",
+                "name": "Калуга"
+            },
+            "salary": {
+                "gross": true,
+                "from": 26000,
+                "to": 35000,
+                "currency": "RUR"
+            },
+            "premium": false,
+            "response_letter_required": false,
+            "type": {
+                "id": "open",
+                "name": "Открытая"
+            },
+            "published_at": "2018-10-17T21:26:36+0300",
+            "employer": {
+                "alternate_url": "https://hh.ru/employer/4934",
+                "trusted": true,
+                "id": "4934",
+                "logo_urls": {
+                    "90": "https://hhcdn.ru/employer-logo/1057274.jpeg",
+                    "240": "https://hhcdn.ru/employer-logo/1057275.jpeg",
+                    "original": "https://hhcdn.ru/employer-logo-original/231258.jpg"
+                },
+                "url": "https://api.hh.ru/employers/4934",
+                "name": "Билайн",
+                "vacancies_url": "https://api.hh.ru/vacancies?employer_id=4934"
+            },
+            "snippet": {
+                "responsibility": "Помогать нашим клиентам разобраться с услугами мобильной связи. Делиться своей экспертизой и знаниями, консультируя клиентов и помогая им решать самые...",
+                "requirement": "Я общительный и любознательный. Нахожу плюсы в любой ситуации и заряжаю позитивом окружающих меня людей. Умею найти выход в самой..."
+            },
+            "id": "27221203",
+            "url": "https://api.hh.ru/vacancies/27221203?host=career.ru",
+            "name": "Оператор Колл-центра (Без опыта)",
+            "apply_alternate_url": "https://hh.ru/applicant/vacancy_response?vacancyId=27221203",
+            "contacts": {
+                "email": "dbolog@rnd.beeline.ru",
+                "phones": [
+                    {
+                        "comment": null,
+                        "country": "7",
+                        "number": "2976050",
+                        "city": "961"
+                    }
+                ],
+                "name": "Болог Дарья Юрьевна"
+            },
+            "address": {
+                "metro": null,
+                "id": "176286",
+                "lng": 0,
+                "raw": "Калуга, ул.Московская, д.289",
+                "lat": 0,
+                "description": null,
+                "street": null,
+                "metro_stations": [],
+                "city": null,
+                "building": null
+            },
+            "created_at": "2018-10-17T21:26:36+0300",
+            "alternate_url": "https://career.ru/vacancy/27221203",
+            "has_test": false,
+            "response_url": null,
+            "relations": [],
+            "sort_point_distance": null,
+            "archived": false,
+            "department": {
+                "id": "bil-4934-beel",
+                "name": "Билайн"
+            }
+        }
+    ]
+});
+
 class App extends Component {
+
   render() {
-    return <AdvancedTable/>
+    const { items } = JSON.parse(file);
+    return <AdvancedTable items={items}/>
   }
 }
 
--- src/components/AdvancedTable.js
+++ src/components/AdvancedTable.js
@@ -1,44 +1,45 @@
 import React from 'react';
 import { Paper, Table, TableHead, TableRow, TableCell, TableBody } from "@material-ui/core";
 
-let id = 0;
-const createData = (name, calories, fat, carbs, protein) => {
-  id += 1;
-  return { id, name, calories, fat, carbs, protein };
-};
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export const AdvancedTable = props => {
+export const AdvancedTable = ({ items }) => {
   return (
     <Paper>
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell numeric>Calories</TableCell>
-            <TableCell numeric>Fat (g)</TableCell>
-            <TableCell numeric>Carbs (g)</TableCell>
-            <TableCell numeric>Protein (g)</TableCell>
+            <TableCell>Должность</TableCell>
+            <TableCell>Оклад</TableCell>
+            <TableCell>Организация разметившая вакансию</TableCell>
+            <TableCell>Контактное лицо</TableCell>
+            <TableCell>Телефон</TableCell>
+            <TableCell>Тип занятости</TableCell>
+            <TableCell>Описание вакансии</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map(row => {
+          {items.map(item => {
             return (
-              <TableRow key={row.id}>
+              <TableRow key={item.id}>
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {item.name}
                 </TableCell>
-                <TableCell numeric>{row.calories}</TableCell>
-                <TableCell numeric>{row.fat}</TableCell>
-                <TableCell numeric>{row.carbs}</TableCell>
-                <TableCell numeric>{row.protein}</TableCell>
+                <TableCell>
+                  {item.salary ? `От ${item.salary.from} до ${item.salary.to}` : "Не указано"}
+                </TableCell>
+                <TableCell>{item.department.name}</TableCell>
+                <TableCell>{item.contacts.name}</TableCell>
+                <TableCell>
+                  {
+                    item.contacts.phones.map((phone, index) => (
+                      <span key={index}>
+                        {`+${phone.country}-${phone.city}-${phone.number}`}<br/>
+                      </span>)
+                    )
+                  }
+
+                </TableCell>
+                <TableCell>{item.contacts.name}</TableCell>
+                <TableCell>{item.snippet.responsibility}</TableCell>
               </TableRow>
             );
           })}
